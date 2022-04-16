e5c63f4ecaae3ace929907cb1d2a56c8d1953fb0
Miti <afrina270@gmail.com>
2022-04-16 12:55:33 +0600

    staff model

--- backend/app/Models/transfer_application.php
+++ backend/app/Models/transfer_application.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class transfer_application extends Model
 {
-    //transfer_application
+    //transfer_application  11 0
     use HasFactory;
     public $timestamps=false;
     
